fa80f75eb8f1e804a747665aa781d4340e2be9d5
ALex <S4519040@outlook.com>
2023-10-24 19:25:21 +0300

    int array add

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
+import java.util.List;
+
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("Zoi zhiv");
         int sqrNine = 3^2;
         System.out.print(sqrNine);
+        int[] nums = new int[]{1, 2, 3, 4, 5};
+
+
     }
 }

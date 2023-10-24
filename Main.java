a22cbdfac0583aedb3350d3a96f5a9c0745d92b4
ALex <S4519040@outlook.com>
2023-10-24 19:20:52 +0300

    sqr Nine count

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.print("Zoi zhiv");
+        int sqrNine = 3^2;
+
     }
 }
